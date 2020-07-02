0c0ccc0fa284ca812b0b730cfee2e72417a51bd4
ABackerNINI <1105908317@qq.com>
2020-07-02 16:56:51 +0800

    update

--- NINI/RamDisk/RamDiskTaskInstall.cmd
+++ NINI/RamDisk/RamDiskTaskInstall.cmd
@@ -1,7 +1,7 @@
 @echo off
 
 REM CREATE RamDisk using ImDisk
-WinTaskScheduler.exe "RamDiskCreate" "C:\NINI\RamDisk\RamDiskCreate.cmd" ""
+WinTaskScheduler.exe "RamDiskCreate" "C:\NINI\RamDisk\RamDiskCreate.cmd"
 REM WinTaskScheduler.exe "RamDisk" "C:\NINI\RamDisk\UACRunner.exe" "imdisk -a -s 16435M -p \\\"/fs:ntfs /q /y\\\" -m R:"
 
 REM RUN RamDiskUserInit in 5 seconds after login
